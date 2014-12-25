c3536f039f56fbbab9eef6502aef336d6d8f5c53
Michal Špaček <tupinek@gmail.com>
2014-12-25 18:26:35 +0100

    Add Map::Tube::GraphViz, Map::Tube::Text::Table, Map::Tube::Bucharest, Map::Tube::Dnipropetrovsk, Map::Tube::Novosibirsk, Map::Tube::Prague, Map::Tube::SaintPetersburg, Map::Tube::Tbilisi, Map::Tube::Vienna and Map::Tube::Yekaterinburg to SEE ALSO section in doc.

--- Kazan.pm
+++ Kazan.pm
@@ -194,19 +194,30 @@ L<namespace::clean>.
 =head1 SEE ALSO
 
 L<Map::Tube>,
+L<Map::Tube::GraphViz>,
+L<Map::Tube::Text::Table>.
+
 L<Map::Tube::Barcelona>,
 L<Map::Tube::Berlin>,
+L<Map::Tube::Bucharest>,
 L<Map::Tube::Delhi>,
+L<Map::Tube::Dnipropetrovsk>,
 L<Map::Tube::Kharkiv>,
 L<Map::Tube::Kiev>,
 L<Map::Tube::London>,
 L<Map::Tube::Minsk>,
 L<Map::Tube::Moscow>,
+L<Map::Tube::Novosibirsk>,
 L<Map::Tube::NYC>,
+L<Map::Tube::Prague>,
+L<Map::Tube::SaintPetersburg>,
 L<Map::Tube::Samara>,
 L<Map::Tube::Sofia>,
+L<Map::Tube::Tbilisi>,
 L<Map::Tube::Tokyo>,
-L<Map::Tube::Warsaw>.
+L<Map::Tube::Vienna>,
+L<Map::Tube::Warsaw>,
+L<Map::Tube::Yekaterinburg>.
 
 =head1 REPOSITORY
 
