a52a0c0f04ee1f5b33f1e612d636089c9b9bfc89
Michal Špaček <tupinek@gmail.com>
2014-12-25 18:24:52 +0100

    Add EXAMPLE3 section to doc.

--- Kazan.pm
+++ Kazan.pm
@@ -146,6 +146,44 @@ For more information about Kazan Map, click L<here|https://en.wikipedia.org/wiki
  # Output like:
  # XML file: .*/kazan-map.xml
 
+=head1 EXAMPLE3
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Map::Tube::GraphViz;
+ use Map::Tube::GraphViz::Utils qw(node_color_without_label);
+ use Map::Tube::Kazan;
+
+ # Object.
+ my $obj = Map::Tube::Kazan->new;
+
+ # GraphViz object.
+ my $g = Map::Tube::GraphViz->new(
+         'callback_node' => \&node_color_without_label,
+         'driver' => 'neato',
+         'tube' => $obj,
+ ); 
+
+ # Get graph to file.
+ $g->graph('Kazan.png');
+
+ # Print file.
+ system "ls -l Kazan.png";
+
+ # Output like:
+ # -rw-r--r-- 1 skim skim 27046 Dec 25 18:23 Kazan.png
+
+=begin html
+
+<a href="https://raw.githubusercontent.com/tupinek/Map-Tube-Kazan/master/images/Kazan.png">
+  <img src="https://raw.githubusercontent.com/tupinek/Map-Tube-Kazan/master/images/Kazan.png" alt="Казанский метрополитен" width="300px" height="300px" />
+</a>
+
+=end html
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
