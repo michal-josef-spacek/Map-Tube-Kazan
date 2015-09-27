fb9748c54042a9f7f25036be05e1e808a6ecdacf
Michal Špaček <tupinek@gmail.com>
2015-09-27 18:56:09 +0200

    Add example for print of metro lines to doc.

--- Kazan.pm
+++ Kazan.pm
@@ -184,6 +184,28 @@ For more information about Kazan Map, click L<here|https://en.wikipedia.org/wiki
 
 =end html
 
+=head1 EXAMPLE4
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Map::Tube::Kazan;
+
+ # Object.
+ my $obj = Map::Tube::Kazan->new;
+
+ # Get lines.
+ my $lines_ar = $obj->get_lines;
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } sort @{$lines_ar};
+
+ # Output:
+ # Центральная линия
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
