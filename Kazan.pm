39371f00466e52af1a9b01b9fdf0d48c26577d74
Michal Josef Špaček <tupinek@gmail.com>
2017-11-10 08:05:43 +0100

    Update author name

--- Kazan.pm
+++ Kazan.pm
@@ -293,13 +293,13 @@ L<https://github.com/tupinek/Map-Tube-Kazan>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2014-2015 Michal Špaček
+ © 2014-2015 Michal Josef Špaček
  Artistic License
  BSD 2-Clause License
 
