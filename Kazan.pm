0aa8795d3528c631cb181a5188c459159f7ad01a
Michal Josef Špaček <tupinek@gmail.com>
2017-11-23 08:45:42 +0100

    Prepare for 0.06 version

--- Kazan.pm
+++ Kazan.pm
@@ -11,7 +11,7 @@ use Moo;
 use namespace::clean;
 
 # Version.
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # Get XML.
 has xml => (
@@ -305,6 +305,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
